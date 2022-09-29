d35fc9d7a4eb47ca31cf597c6eddd14fce8a9cce
Bloark <wildson.luiz@gmail.com>
2022-09-29 17:55:02 -0300

    Implementado Método Get ObterPorId

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -18,10 +18,13 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            // TODO: Buscar o Id no banco utilizando o EF
+            // TODO: Buscar o Id no banco utilizando o EF - OK
+            var tarefa = _context.Tarefas.Find(id);
             // TODO: Validar o tipo de retorno. Se não encontrar a tarefa, retornar NotFound,
+            if(tarefa == null)
+                return NotFound();
             // caso contrário retornar OK com a tarefa encontrada
-            return Ok();
+            return Ok(tarefa);
         }
 
         [HttpGet("ObterTodos")]
